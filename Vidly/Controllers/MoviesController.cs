0b7c9c4aed705b630b8e8fed0996c7f3778e7360
Tikumporn Wankvar <tikumporn.w@pea.co.th>
2017-08-16 17:32:54 +0700

    First Commiut

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Sherk!!" };
+
+            //return View(movie);
+            //return Content("Hello World!!");
+            //return HttpNotFound();
+            //return new EmptyResult();
+            return RedirectToAction("Index", "Home", new { page=1,sortBy= "name" });
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+        // movies
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+            
+        }
+        public ActionResult ByRealeaseDate(int year, int month)
+        {
+            return View();
+        }
+    }
+}
